4091ee9dbcf2ebca6fa6b7181f34204d66f59bd8
rsgrava <rsgrava@hotmail.com>
2023-05-26 11:51:12 -0300

    states: create main state template

---
+++ src/states/menu.lua
@@ -0,0 +1,19 @@
+menu = {}
+
+function menu:init()
+end
+
+function menu:enter()
+end
+
+function menu:leave(dt)
+end
+
+function menu:resume(dt)
+end
+
+function menu:update(dt)
+end
+
+function menu:draw()
+end
