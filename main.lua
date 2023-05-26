4751fb042294f0182294b484399c618b86001277
rsgrava <rsgrava@hotmail.com>
2023-05-26 10:51:49 -0300

    main: setup window

--- main.lua
+++ main.lua
@@ -1,5 +1,13 @@
-function love.load()
+push = require("push")
+require "src/constants"
 
+function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    local window_w, window_h = love.window.getDesktopDimensions()
+    push:setupScreen(GAME_W, GAME_H, window_w, window_h,
+                     {fullscreen = true, resizable = true, vsync = true})
+    love.window.setTitle("love")
+    math.randomseed(os.time())
 end
 
 function love.update(dt)
@@ -7,5 +15,10 @@ function love.update(dt)
 end
 
 function love.draw()
+    push:start()
+    push:finish()
+end
 
+function love.resize(w, h)
+  push:resize(w, h)
 end
