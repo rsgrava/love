c273fc1060c4e1cd91183e3ff329160e00c0af41
rsgrava <rsgrava@hotmail.com>
2023-05-28 19:02:19 -0300

    entities: map: implement tile layers

---
+++ src/entities/map.lua
@@ -0,0 +1,73 @@
+Class = require("libs/class")
+require "src/constants"
+require "src/utils"
+
+Map = Class{}
+
+function Map:init(map)
+    self.tilesets = {}
+    self.tileset_quads = {}
+    self.layers = {}
+    self.tileset_ids = {}
+    self.collision = {}
+    self.width = map.width
+    self.height = map.height
+
+    for k, tileset in ipairs(map.tilesets) do
+        tileset = require("assets/tilesets/"..tileset.name)
+        self.tilesets[k] = assets.graphics[tileset.name]
+        self.tileset_quads[k] = generateQuads(self.tilesets[k])
+    end
+
+    for layer_id, layer in ipairs(map.layers) do
+        if layer.type == "tilelayer" then
+            if layer.name == "collision" then
+
+            else
+                self.layers[layer_id] = {}
+                self.tileset_ids[layer_id] = {}
+                for tile_id, tile in ipairs(layer.data) do
+                    for tileset_id, tileset in ipairs(map.tilesets) do
+                        if tile == 0 then
+                            self.layers[layer_id][tile_id] = 0
+                            self.tileset_ids[layer_id][tile_id] = 0
+                            break
+                        elseif tile < tileset.firstgid then
+                            self.layers[layer_id][tile_id] = tile - map.tilesets[tileset_id - 1].firstgid
+                            self.tileset_ids[layer_id][tile_id] = tileset_id - 1
+                            break
+                        elseif tileset_id == #map.tilesets then
+                            self.layers[layer_id][tile_id] = tile - map.tilesets[tileset_id].firstgid
+                            self.tileset_ids[layer_id][tile_id] = tileset_id
+                            break
+                        end
+                    end
+                end
+            end
+        elseif layer.type == "objects" then
+        end
+    end
+end
+
+function Map:update(dt)
+end
+
+function Map:draw()
+    for layer_id, layer in ipairs(self.layers) do
+        for tile_id, tile in ipairs(layer) do
+            if tile ~= 0 then
+                local tileset_id = self.tileset_ids[layer_id][tile_id]
+                local texture = self.tilesets[tileset_id]
+                local quad = self.tileset_quads[tileset_id][tile]
+                local x = (tile_id - 1) % self.width
+                local y = math.floor((tile_id - 1) / self.width)
+                love.graphics.draw(
+                    texture,
+                    quad,
+                    x * TILE_W,
+                    y * TILE_H
+                )
+            end
+        end
+    end
+end
