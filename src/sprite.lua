bc7c24dab06ff8bccf4cd51da0d0c823c62c76b9
rsgrava <rsgrava@hotmail.com>
2023-05-29 13:02:57 -0300

    sprite: implement Sprite class

---
+++ src/sprite.lua
@@ -0,0 +1,28 @@
+Class = require("libs/class")
+require("src/utils")
+require("src/animation")
+
+Sprite = Class{}
+
+function Sprite:init(defs)
+    self.texture = defs.texture
+    self.animation = Animation(defs.animation, defs.firstAnim)
+    self.quads = generateQuads(defs.texture, defs.width, defs.height)
+end
+
+function Sprite:setAnimation(anim)
+    self.animation:setAnimation(anim)
+end
+
+function Sprite:update(dt)
+    self.animation:update(dt)
+end
+
+function Sprite:draw(x, y)
+    love.graphics.draw(
+        self.texture,
+        self.quads[self.animation:getFrame()],
+        x,
+        y
+    )
+end
