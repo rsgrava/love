57528361f0c41ae867996a603daf9af40e14c4ca
rsgrava <rsgrava@hotmail.com>
2023-06-09 15:47:32 -0300

    common_events: create common events compilation

---
+++ src/common_events.lua
@@ -0,0 +1,23 @@
+function wait(self, dt, time)
+    self.timer = 0
+    ::wait::
+    coroutine.yield()
+    self.timer = self.timer + dt
+    if self.timer < time then
+        goto wait
+    end
+end
+
+function teleportPlayer(self, dt, mapName, x, y, dir)
+    disableControls()
+        ScreenEffects.fadeOut(0.2)
+            wait(self, dt, 0.3)
+            local mapName = self.props.map
+            if Map.name ~= MapName then
+                Map.load(assets.maps[mapName])
+            end
+            local player = ActorManager.getPlayer()
+            player:setPosition(x, y, dir)
+        ScreenEffects.fadeIn(0.2)
+    enableControls()
+end
